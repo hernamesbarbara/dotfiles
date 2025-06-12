9c864cb766760212374c00cc2d0e09616d0124ec
austin <tips@cia.lol>
2025-06-12 14:25:33 -0600

    ls5 and ls10

--- aliases.zsh
+++ aliases.zsh
@@ -9,6 +9,9 @@ alias chrome="open -a Google\ Chrome"
 alias brave="open -a Brave\ Browser"
 alias rmpyc="find . -name '*.pyc' -type f -delete"
 
+alias ls5="ls -lt --color=always | head -n 5"
+alias ls10="ls -lt --color=always | head -n 10"
+
 alias today='printf "$(date "+%Y-%m-%d")"'
 
 alias utms='printf "?utm_source=medium.com&utm_medium=blog&utm_campaign=austin&utm_content="'
